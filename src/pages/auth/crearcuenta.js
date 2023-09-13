d28409131116a802802264672806a2ff2dc6ed80
ForestParadiseOrigene <andres.jcv.678@gmail.com>
2023-09-13 15:03:01 -0500

    cositaaaaas

--- src/pages/auth/crearcuenta.js
+++ src/pages/auth/crearcuenta.js
@@ -1,8 +1,53 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import { Link } from "react-router-dom";
+import APIInvoke  from "../../utils/APIInvoke";
+import swal from 'sweetalert';
+
 const CrearCuenta = () => {
+
+    const [usuario , setUsuario] = useState(
+        {
+            nombre:'',
+            email:'',
+            password:'',
+            confirmar:''
+        }
+    );
+    
+    
+    const { nombre, email, password, confirmar } = usuario;
+
+    const onChange = (e) => {
+        setUsuario(
+            {
+                ...usuario,
+                [e.target.name]: e.target.value
+            }
+        )
+    }
+
+    useEffect(()=> {
+        document.getElementById("nombre").focus();
+    }, [])
+
+    const crearCuenta = async () => {
+        const data = {
+            nombre:usuario.nombre,
+            email: usuario.email,
+            password: usuario.password
+        }
+        const response = await APIInvoke.invokePOST(`/usuario`, data);
+        console.log(response)
+    }
+
+    const onSubmit = (e) => {
+        e.preventDefault();
+        crearCuenta();
+    }
+
     return (
-        <div class="hold-transition register-page">
+
+        <div className = "hold-transition register-page">
             <div className="register-box">
                 <div className="register-logo">
                     <Link to={"#"}><b>Crear Cuenta</b></Link>
@@ -10,33 +55,70 @@ const CrearCuenta = () => {
                 <div className="card">
                     <div className="card-body register-card-body">
                         <p className="login-box-msg">Crear tu nuevo usuario, ¡ahora!</p>
-                        <form action="../../index.html" method="post">
+
+                        <form onSubmit={onSubmit}>
+
                             <div className="input-group mb-3">
-                                <input type="text" className="form-control" placeholder="Nombre completo" />
+                                <input 
+                                    id="nombre"
+                                    name="nombre"
+                                    placeholder="Nombre completo" 
+                                    value={nombre}
+                                    onChange={onChange}
+                                    type="text"
+                                    className="form-control"
+                                />
                                 <div className="input-group-append">
                                     <div className="input-group-text">
                                         <span className="fas fa-user" />
                                     </div>
                                 </div>
                             </div>
+
                             <div className="input-group mb-3">
-                                <input type="email" className="form-control" placeholder="Correo electronico" />
+                                <input
+                                    id="email"
+                                    name="email"
+                                    type="email"
+                                    value={email}
+                                    onChange={onChange}
+                                    placeholder="Correo electronico"
+                                    className="form-control"
+                                />
                                 <div className="input-group-append">
                                     <div className="input-group-text">
                                         <span className="fas fa-envelope" />
                                     </div>
                                 </div>
                             </div>
+                            
                             <div className="input-group mb-3">
-                                <input type="password" className="form-control" placeholder="Contraseña" />
+                                <input
+                                    id="password"
+                                    name="password"
+                                    value={password}
+                                    onChange={onChange}
+                                    type="password"
+                                    className="form-control"
+                                    placeholder="Contraseña"
+                                />
                                 <div className="input-group-append">
                                     <div className="input-group-text">
                                         <span className="fas fa-lock" />
                                     </div>
                                 </div>
                             </div>
+
                             <div className="input-group mb-3">
-                                <input type="password" className="form-control" placeholder="Repita su contraseña" />
+                                <input
+                                    id="confirmar"
+                                    name="confirmar"
+                                    type="password"
+                                    value={confirmar}
+                                    onChange={onChange}
+                                    className="form-control"
+                                    placeholder="Confirme su contraseña"
+                                />
                                 <div className="input-group-append">
                                     <div className="input-group-text">
                                         <span className="fas fa-lock" />
@@ -46,8 +128,11 @@ const CrearCuenta = () => {
                         
                         <div className="social-auth-links text-center">
                             
-                            <button to={"#"} className="btn btn-block btn-primary">
-                                
+                            <button
+                                to={"#"}
+                                className="btn btn-block btn-primary"
+                                type="submit"
+                            >
                                 Crear Cuenta
                             </button>
                             <Link to={"/"} className="btn btn-block btn-danger">
@@ -62,7 +147,8 @@ const CrearCuenta = () => {
             </div>
 
 
-        </div>);
+        </div>
+        );
 }
 
 export default CrearCuenta;
